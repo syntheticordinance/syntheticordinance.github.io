bb77e39d416697ffc1e3d8c8d9ed55ca2c33f10a
Stephen <synthetically.ordinary@gmail.com>
2020-05-24 22:06:45 -0700

    Removed test page; grammatical fix.

--- TestPage.js
+++
@@ -1,7 +0,0 @@
-var pw = prompt("Please enter authorisation code.", "Harry Potter");
-
-if (pw == null || pw == "Harry Potter") {
-  txt = "Contact: 65305642+syntheticordinance@users.noreply.github.com";
-} else {
-  txt = "Error.";
-}
